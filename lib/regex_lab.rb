5b62b8b797bba11f4ba59fe31d0576ebc6c4d59d
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-21 08:44:13 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.match(/\b[aeiouAEIOU]/)
-    return true
-  else
-    return false
-  end
+  word.match(/\b[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -15,11 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/[A-Z]+\w+[.!?,;:]/)
-    return true
-  else
-    return false
-  end
+  text.match(/[A-Z]+\w+[.!?,;:]/)
 end
 
 def valid_phone_number?(phone)
